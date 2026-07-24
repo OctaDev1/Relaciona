0031840f5c85eee0730e1861aa15a8d293a04d86
João Vitor Diniz Alves <diniz.vitor.alves@gmail.com>
2026-07-24 09:00:56 -0300

    :tada: Primeiro Commit

---
+++ src/main/java/com/generation/relaciona/RelacionaApplication.java
@@ -0,0 +1,13 @@
+package com.generation.relaciona;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RelacionaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RelacionaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/generation/relaciona/RelacionaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.relaciona;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RelacionaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
