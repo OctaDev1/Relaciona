49a0f37863c95f2adc389bbcc3b17027c36c1763
João Vitor Diniz Alves <diniz.vitor.alves@gmail.com>
2026-07-24 11:33:10 -0300

    :sparkles: Atualizar Cliente Criado

--- src/main/java/com/generation/relaciona/controller/ClienteController.java
+++ src/main/java/com/generation/relaciona/controller/ClienteController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -46,6 +47,12 @@ public class ClienteController {
         return ResponseEntity.status (HttpStatus.CREATED)
                 .body(clienteRepository.save(cliente));
     }
-
+	
+	 @PutMapping
+	    public ResponseEntity<Cliente>put(@RequestBody Cliente cliente){
+	        if (clienteRepository.existsById (cliente.getId ()))
+	            return ResponseEntity.ok (clienteRepository.save (cliente));
+	        return ResponseEntity.notFound ().build ();
+	    }
 
 }
