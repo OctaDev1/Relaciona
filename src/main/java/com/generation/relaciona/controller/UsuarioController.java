0a5daf8bbbd4696abdb383ee3bae6e41b60603cb
Giovanna <mennezes.giovanna@gmail.com>
2026-07-24 10:51:05 -0300

    feat: recurso Usuario

---
+++ src/main/java/com/generation/relaciona/controller/UsuarioController.java
@@ -0,0 +1,66 @@
+package com.generation.relaciona.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import com.generation.relaciona.model.Usuario;
+import com.generation.relaciona.repository.UsuarioRepository;
+
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("/usuarios")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+public class UsuarioController {
+	@Autowired
+	private UsuarioRepository usuarioRepository;
+	
+	@GetMapping
+	public ResponseEntity<List<Usuario>> getAll(){
+		return ResponseEntity.ok(usuarioRepository.findAll());
+	}
+	
+	@GetMapping("/descricao/{descricao}")
+	public ResponseEntity<List<Usuario>> getAllByDescricao(String nome){
+		return ResponseEntity.ok(usuarioRepository.findAllByNomeContainingIgnoreCase(nome));
+	}
+	
+	@PostMapping
+	public ResponseEntity<Usuario> post(@Valid @RequestBody Usuario usuario){
+		return ResponseEntity.status(HttpStatus.CREATED).body(usuarioRepository.save(usuario));
+	}
+	
+	@PutMapping
+	public ResponseEntity<Usuario> put(@Valid @RequestBody Usuario usuario){
+		if(usuarioRepository.existsById(usuario.getId())) {
+			return ResponseEntity.ok(usuarioRepository.save(usuario));
+		}
+		throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Este usuario não existe.", null);
+	}
+	
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	@DeleteMapping("/{id}")
+	public void delete(@PathVariable Long id) {
+		Optional<Usuario> usuario = usuarioRepository.findById(id);
+		
+		if(usuario.isEmpty()) {
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+		}
+		usuarioRepository.deleteById(id);
+	}
+}
